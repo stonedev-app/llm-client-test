e9c243a1ea53aaa2c78d374cc847cae6fa6d8284
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-10-13 10:26:27 +0900

    fix: modern-normalizeのimportを一番上に移動

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+// CSS
+import "modern-normalize";
+
+// JavaScript
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "modern-normalize";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
