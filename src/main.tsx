e825418e6a7ba4475a272fa6349eba10ac359714
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-10-13 09:56:02 +0900

    feat: main.tsxにリセットCSSを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "modern-normalize";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
