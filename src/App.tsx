9dfa994d1465218823632e69b9c233b00200d73d
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-10-18 17:51:33 +0900

    feat: テキストエリアを画面に追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Textarea } from "./components/ui/textarea";
 import { Button } from "./components/ui/button";
 
 export const App = () => {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center gap-4 bg-gray-100">
-      <Button variant="outline">Button</Button>
+    <div className="flex min-h-screen items-center justify-center p-8 bg-gray-100">
+      <div className="flex flex-col gap-4 w-full">
+        <Textarea placeholder="Type your message here." className="bg-white" />
+        <div className="flex justify-end">
+          <Button variant="outline">Send</Button>
+        </div>
+      </div>
     </div>
   );
 };
