b8fb1857311dc3fb6f6bf286fa8915e2ab9b57a7
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-10-19 10:10:00 +0900

    feat: 入力メッセージの処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from "react";
+
 import { Textarea } from "./components/ui/textarea";
 import { Button } from "./components/ui/button";
 
 export const App = () => {
+  // 入力メッセージ
+  const [inputMessage, setInputMessage] = useState<string>("");
+  // 送信ボタン無効
+  const [sendBtnDisabled, setSendBtnDisabled] = useState<boolean>(true);
+
+  // 入力メッセージが変更された場合
+  useEffect(() => {
+    // 入力メッセージ欄に何か入力されていれば、送信ボタンを有効にする
+    setSendBtnDisabled(!inputMessage.trim());
+  }, [inputMessage]);
+
   return (
     <div className="flex min-h-screen items-center justify-center p-8 bg-gray-100">
       <div className="flex flex-col gap-4 w-full max-w-2xl">
-        <Textarea placeholder="Type your message here." className="bg-white" />
+        <Textarea
+          placeholder="Type your message here."
+          className="bg-white"
+          value={inputMessage}
+          onChange={(e) => {
+            setInputMessage(e.currentTarget.value);
+          }}
+        />
         <div className="flex justify-end">
-          <Button variant="outline">Send</Button>
+          <Button variant="outline" disabled={sendBtnDisabled}>
+            Send
+          </Button>
         </div>
       </div>
     </div>
