d0327dbe292cc29b132638851b046c2c03f68cac
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-10-31 19:32:51 +0900

    feat: リセットCSSを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import Button from "@mui/material/Button";
 
 export function App() {
   return (
     <>
+      {/* リセットCSSを適用 */}
+      <CssBaseline />
+      <p>App.txs</p>
       <Button variant="contained">Hello world</Button>
     </>
   );
