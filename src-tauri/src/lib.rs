e081ff31752bc1979ab59a490c2d2976960e19cf
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-02 10:06:58 +0900

    feat: ollamaの呼び出し処理を追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,39 @@
+use reqwest::Client;
+use serde_json::json;
+
+#[tauri::command]
+async fn call_ollama(prompt: String) -> Result<String, String> {
+    let client = Client::new();
+
+    let body = json!({
+        "model": "llama3",
+        "prompt": prompt
+    });
+
+    let res = match client
+        .post("http://localhost:11434/api/generate")
+        .json(&body)
+        .send()
+        .await
+    {
+        Ok(response) => response,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let text = match res.text().await {
+        Ok(t) => t,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    return Ok(text);
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![call_ollama])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
