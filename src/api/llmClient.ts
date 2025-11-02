1d47395c1a89f5a5de2d32ef19c23786bb47c093
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-02 17:21:50 +0900

    fix: invokeのエラーは、try catchを使わないと、エラー内容を受け取れないので修正

--- src/api/llmClient.ts
+++ src/api/llmClient.ts
@@ -6,14 +6,26 @@ export const requestLLM = async (
   message: string,
   setMessages: Dispatch<SetStateAction<Message[]>>
 ) => {
-  const resMessage: string = await invoke("request_llm", { prompt: message });
-  // メッセージ配列に応答メッセージを追加して再設定
-  setMessages((prev) => [
-    ...prev,
-    {
-      id: prev.length + 1,
-      text: resMessage,
-      fromMe: false,
-    },
-  ]);
+  try {
+    const resMessage: string = await invoke("request_llm", { prompt: message });
+    // メッセージ配列に応答メッセージを追加して再設定
+    setMessages((prev) => [
+      ...prev,
+      {
+        id: prev.length + 1,
+        text: resMessage,
+        fromMe: false,
+      },
+    ]);
+  } catch (err) {
+    // メッセージ配列にエラーメッセージを追加して再設定
+    setMessages((prev) => [
+      ...prev,
+      {
+        id: prev.length + 1,
+        text: err as string,
+        fromMe: false,
+      },
+    ]);
+  }
 };
