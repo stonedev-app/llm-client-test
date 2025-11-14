1b13e5a4b3f85ebb1518fe3dcc835e4cbcffbac0
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-15 07:20:38 +0900

    feat: ログ出力のライブラリを追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ use llm::ollama_api_chat;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![ollama_api_chat])
