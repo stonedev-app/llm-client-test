4abf09d399c61cb97899d9676e99f421c03d9219
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-15 08:42:05 +0900

    feat: ログ出力の設定を追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,13 +3,27 @@ mod llm;
 
 use llm::ollama_api_chat;
 
+// ログ出力先
+// Linux    $XDG_DATA_HOME/{bundleIdentifier}/logs
+//          $HOME/.local/share/{bundleIdentifier}/logs
+// 例       /home/alice/.local/share/com.tauri.dev/logs
+// macOS    {homeDir}/Library/Logs/{bundleIdentifier}
+// 例       /Users/Alice/Library/Logs/com.tauri.dev
+// Windows  {FOLDERID_LocalAppData}/{bundleIdentifier}/logs
+// 例       C:\Users\Alice\AppData\Local\com.tauri.dev\logs
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(
             tauri_plugin_log::Builder::new()
-                .level(log::LevelFilter::Info) // Info以上をログ出力
+                // Info以上をログ出力
+                .level(log::LevelFilter::Info)
+                // ログ最大ファイルサイズを設定
+                .max_file_size(50_000 /* bytes */)
+                // ログ日付にローカル・タイムゾーンを設定
+                .timezone_strategy(tauri_plugin_log::TimezoneStrategy::UseLocal)
                 .build(),
         )
         .plugin(tauri_plugin_opener::init())
