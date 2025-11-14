3847771f3f7dcd8e1fa43660fc29f15f0324999a
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-11-15 07:33:45 +0900

    fix: ログが出力されすぎるので、info以上に設定

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,11 @@ use llm::ollama_api_chat;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(log::LevelFilter::Info) // Info以上をログ出力
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![ollama_api_chat])
